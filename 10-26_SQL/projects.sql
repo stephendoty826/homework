1226ca25c2ac11a5de597d99394f6117049109b7
Stephen Doty <stephendoty826@yahoo.com>
2021-10-26 22:19:57 -0500

    finished homework for 10-26-21

---
+++ 10-26_SQL/projects.sql
@@ -0,0 +1,220 @@
+-- SET statement_timeout = 0;
+-- SET lock_timeout = 0;
+-- SET client_encoding = 'UTF8';
+-- SET standard_conforming_strings = on;
+-- SET check_function_bodies = false;
+-- SET client_min_messages = warning;
+-- CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+-- COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+-- SET search_path = public, pg_catalog;
+-- SET default_tablespace = '';
+-- SET default_with_oids = false;
+-- --
+-- -- Name: project; Type: TABLE; Schema: public; Owner: airportyh; Tablespace:
+-- --
+-- CREATE TABLE project (
+--     id integer NOT NULL,
+--     name character varying
+-- );
+-- CREATE SEQUENCE project_id_seq
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
+-- ALTER SEQUENCE project_id_seq OWNED BY project.id;
+-- CREATE TABLE project_uses_tech (
+--     project_id integer,
+--     tech_id integer
+-- );
+-- CREATE TABLE tech (
+--     id integer NOT NULL,
+--     name character varying
+-- );
+-- CREATE SEQUENCE tech_id_seq
+--     START WITH 1
+--     INCREMENT BY 1
+--     NO MINVALUE
+--     NO MAXVALUE
+--     CACHE 1;
+-- ALTER SEQUENCE tech_id_seq OWNED BY tech.id;
+-- ALTER TABLE ONLY project ALTER COLUMN id SET DEFAULT nextval('project_id_seq'::regclass);
+-- ALTER TABLE ONLY tech ALTER COLUMN id SET DEFAULT nextval('tech_id_seq'::regclass);
+-- COPY project (id, name) FROM stdin;
+-- 1	School Bus
+-- 2	Medium Blog Layout
+-- 3	Mozilla Front Page
+-- 4	Personal Website
+-- 5	Modal Dialog
+-- 6	CSS Tricks Blog Layout
+-- 7	Whiteboard Exercises
+-- 8	RPG Hero Game
+-- 9	Catch the Monster Game
+-- 10	Phone Book
+-- 11	Turtle Graphics Exercises
+-- \.
+-- SELECT pg_catalog.setval('project_id_seq', 11, true);
+-- COPY project_uses_tech (project_id, tech_id) FROM stdin;
+-- 1	1
+-- 1	2
+-- 2	1
+-- 2	2
+-- 3	1
+-- 3	2
+-- 3	9
+-- 4	1
+-- 4	2
+-- 5	1
+-- 5	2
+-- 6	1
+-- 6	2
+-- 8	4
+-- 9	4
+-- 9	6
+-- 10	4
+-- 11	4
+-- 11	5
+-- 8	10
+-- 9	10
+-- 10	11
+-- 10	12
+-- \.
+-- COPY tech (id, name) FROM stdin;
+-- 1	HTML
+-- 2	CSS
+-- 3	JavaScript
+-- 4	Python
+-- 5	Turtle Graphics
+-- 6	PyGame
+-- 7	Java
+-- 8	Ruby
+-- 9	Bootstrap
+-- 10	Objects
+-- 11	File IO
+-- 12	Pickle
+-- \.
+-- SELECT pg_catalog.setval('tech_id_seq', 12, true);
+-- ALTER TABLE ONLY project
+--     ADD CONSTRAINT project_pkey PRIMARY KEY (id);
+-- ALTER TABLE ONLY tech
+--     ADD CONSTRAINT tech_pkey PRIMARY KEY (id);
+-- ALTER TABLE ONLY project_uses_tech
+--     ADD CONSTRAINT project_uses_tech_project_id_fkey FOREIGN KEY (project_id) REFERENCES project(id);
+-- ALTER TABLE ONLY project_uses_tech
+--     ADD CONSTRAINT project_uses_tech_tech_id_fkey FOREIGN KEY (tech_id) REFERENCES tech(id);
+-- REVOKE ALL ON SCHEMA public FROM PUBLIC;
+-- GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+
+-- Problems 
+
+-- 1. Select all projects that use JavaScript?
+-- SELECT *
+-- FROM project
+-- INNER JOIN project_uses_tech
+-- ON project.id = project_id
+-- INNER JOIN tech
+-- ON tech_id = tech.id
+-- WHERE tech.name = 'JavaScript'
+
+-- 2. Select all technologies used by the Personal Website?
+-- SELECT tech.name
+-- FROM project
+-- INNER JOIN project_uses_tech
+-- ON project.id = project_id
+-- INNER JOIN tech
+-- ON tech_id = tech.id
+-- WHERE project.name = 'Personal Website'
+
+-- 3. Perform a left outer join from the tech table to the project_uses_tech table - which techs has no associated project?
+-- SELECT *
+-- FROM tech
+-- LEFT OUTER JOIN project_uses_tech
+-- ON tech.id = tech_id
+-- WHERE project_id IS NULL
+
+-- 4. Based on the previous query, get the count of the number of techs used by each project.
+-- SELECT tech.name, COUNT(*)
+-- FROM tech
+-- LEFT OUTER JOIN project_uses_tech
+-- ON tech.id = tech_id
+-- GROUP BY tech.id
+
+-- 5. Perform a left outer join from the project table to the project_users_tech table - which projects has no associated tech?
+-- SELECT *
+-- FROM project
+-- LEFT OUTER JOIN project_uses_tech
+-- ON project.id = project_id
+-- WHERE tech_id IS NULL
+
+-- 6 Based on the previous query, get the count of the number of projects that use each tech.
+-- SELECT tech.name, COUNT(tech.name) AS project_count
+-- FROM project
+-- LEFT OUTER JOIN project_uses_tech
+-- ON project.id = project_id
+-- INNER JOIN tech
+-- ON tech.id = tech_id
+-- GROUP BY tech.name
+
+-- 7. List all projects along with each technology used by it. You will need to do a three-way join.
+-- SELECT project.name, tech.name
+-- FROM project
+-- LEFT OUTER JOIN project_uses_tech
+-- ON project.id = project_id
+-- LEFT OUTER JOIN tech
+-- ON tech.id = tech_id
+
+-- 8. List all the distinct techs that are used by at least one project.
+-- SELECT tech.name
+-- FROM tech
+-- INNER JOIN project_uses_tech
+-- ON tech.id = tech_id
+-- GROUP BY tech.name
+
+-- 9. List all the distinct techs that are not used by any projects.
+-- SELECT tech.name
+-- FROM tech
+-- LEFT OUTER JOIN project_uses_tech
+-- ON tech.id = tech_id
+-- WHERE project_id IS NULL
+
+-- 10. List all the distinct projects that use at least one tech.
+-- SELECT project.name
+-- FROM project
+-- INNER JOIN project_uses_tech
+-- ON project.id = project_id
+-- GROUP BY project.name
+
+-- 11. List all the distinct projects that use no tech.
+-- SELECT *
+-- FROM project
+-- LEFT OUTER JOIN project_uses_tech
+-- ON project.id = project_id
+-- WHERE tech_id IS NULL
+
+-- 12. Order the projects by how many tech it uses.
+-- SELECT project.name, COUNT(tech_id) AS tech_count
+-- FROM project
+-- LEFT OUTER JOIN project_uses_tech
+-- ON project.id = project_id
+-- GROUP BY project.name
+-- ORDER BY COUNT(tech_id) DESC
+
+-- 13. Order the tech by how many projects use it.
+-- SELECT tech.name, COUNT(project_id) AS project_count
+-- FROM tech
+-- LEFT OUTER JOIN project_uses_tech
+-- ON tech.id = tech_id
+-- GROUP BY tech.name
+-- ORDER BY COUNT(project_id) DESC
+
+-- 14. What is the average number of techs used by a project?
+-- SELECT AVG(count) AS average_tech_per_project
+-- FROM ( 
+--     SELECT COUNT(tech_id)
+--     FROM project_uses_tech
+--     LEFT OUTER JOIN tech
+--     ON tech.id = tech_id
+--     GROUP BY project_uses_tech.project_id
+--     ) AS count
---
+++ 10-26_SQL/restaurantsV2.sql
@@ -0,0 +1,154 @@
+
+-- CREATE TABLE restaurant(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR,
+--     address VARCHAR,
+--     category VARCHAR
+-- );
+
+-- CREATE TABLE reviewer(
+--     id SERIAL PRIMARY KEY,
+--     name VARCHAR,
+--     email VARCHAR,
+--     karma NUMERIC CHECK (karma >= 0 and karma <= 7)
+-- ); 
+
+-- CREATE TABLE review(
+--     id SERIAL PRIMARY KEY,
+--     reviewer_id INTEGER REFERENCES reviewer (id),
+--     stars NUMERIC CHECK (stars >= 1 AND stars <= 5),
+--     title VARCHAR(50),
+--     review VARCHAR,
+--     restaurant_id INTEGER REFERENCES restaurant (id)
+-- );
+
+-- INSERT INTO restaurant VALUES 
+--     (DEFAULT, 'Olive Garden', '1234 Almond St Houston TX', 'Italian'),
+--     (DEFAULT, 'Torchys', '2341 Peanut St Houston TX', 'Tex-Mex'),
+--     (DEFAULT, 'Franks Grill', '3412 Walnut Bend Houston TX', 'American'),
+--     (DEFAULT, 'Outback Steakhouse', '4123 Pecan Rd Houston TX',  'American'),
+--     (DEFAULT, 'Pappas BBQ', '4123 Beanwi Rd Houston TX', 'BBQ'),
+--     (DEFAULT, 'Raising Canes', '3124 Pelbri Rd Houston TX', 'Fried Chicken'),
+--     (DEFAULT, 'Garden of Olives', '1231 Groot St Houston TX', 'Italian'),
+--     (DEFAULT, 'Flaming Tacos', '2342 Ole St Houston TX', 'Tex-Mex'),
+--     (DEFAULT, 'Chucks Grill', '3413 Latrick Bend Houston TX', 'American'),
+--     (DEFAULT, 'Salt Grass Steakhouse', '4124 Shuum Rd Houston TX',  'American'),
+--     (DEFAULT, 'Hidaway BBQ', '4125 Swantro Rd Houston TX', 'BBQ'),
+--     (DEFAULT, 'Chick-fil-a', '3126 Helm Rd Houston TX', 'Fried Chicken');
+
+-- INSERT INTO reviewer VALUES
+-- (DEFAULT, 'Karen', 'tookaren@hotmail.com', 2),
+-- (DEFAULT, 'Billy', 'billywilly@hotmail.com', 3),
+-- (DEFAULT, 'Bobby', 'bobbynewportn@hotmail.com', 0),
+-- (DEFAULT, 'Samantha', 'sam185@hotmail.com', 7),
+-- (DEFAULT, 'Krieg', 'psychokrieg@hotmail.com', 1),
+-- (DEFAULT, 'Charles', 'charleschuck@hotmail.com', 4),
+-- (DEFAULT, 'Matt', 'magicmatt@hotmail.com', 6),
+-- (DEFAULT, 'Rick', 'rick@hotmail.com', 5);
+
+-- INSERT INTO review VALUES
+-- (DEFAULT, 1, 1, 'Sooo Rude...', 'The employees were so rude to me.', 1),
+-- (DEFAULT, 2, 3.5, 'Great Food', 'Food was good, atmosphere was OK', 2),
+-- (DEFAULT, 3, 5, 'It was OK', 'It was OK. Food was not too great', 3),
+-- (DEFAULT, 4, 2.5, 'Kinda Rude', 'Food was great. Employees were kinda rude.', 4),
+-- (DEFAULT, 5, 4.5, 'Amazing', 'The food was sooo good. I have to go again.', 5),
+-- (DEFAULT, 6, 3.5, 'Not my style but good food.', 'Atmosphere was not for me but the food was good.', 6),
+-- (DEFAULT, 7, 4, 'Slow service', 'Service was slow but food was good when we got it.', 13),
+-- (DEFAULT, 8, 3, 'Great Experience', 'Food was great and employees were so nice.', 14),
+-- (DEFAULT, 1, 2.5, 'Would not go again', 'Restaurant was dirty and employees were rude. Food was OK.', 15),
+-- (DEFAULT, 2, 2, 'Have to go again.', 'Great food. Wonderfull experience and atmosphere.', 16),
+-- (DEFAULT, 3, 5, 'Why???', 'My only complaint is that this restaurant is on the other side of town.', 17),
+-- (DEFAULT, 4, 4, 'How much food can you eat', 'Food was great. Portions were big.', 18),
+-- (DEFAULT, 5, 3, 'Could not eat another bite', 'I was so full when I left. Gotta go again.', 1),
+-- (DEFAULT, 6, 1, 'No my cup of tea', 'They did not have my kind of tea.', 2),
+-- (DEFAULT, 7, 2, 'Very very rude', 'Employee at the cashier yelled at me. Food was OK.', 3),
+-- (DEFAULT, 8, 1.5, 'Not very good', 'Food and atmosphere was not very good.', 4);
+
+
+-- 4. Answer These Questions With a Query
+    -- 4.1 List all the reviews for a given restaurant given a specific restaurant ID.
+    -- SELECT * 
+    -- FROM review
+    -- WHERE restaurant_id = 1
+
+    -- 4.2 List all the reviews for a given restaurant, given a specific restaurant name.
+    -- SELECT *
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- ON restaurant.id = review.restaurant_id
+    -- WHERE restaurant.name = 'Franks Grill'
+
+    -- 4.3 List all the reviews for a given reviewer, given a specific author name.
+    -- SELECT *
+    -- FROM review
+    -- INNER JOIN reviewer
+    -- ON review.reviewer_id = reviewer.id
+    -- WHERE reviewer.name = 'Bobby'
+
+    -- 4.4 List all the reviews along with the restaurant they were written for. In the query result, select the restaurant name and the review text.
+    -- SELECT name, review 
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- ON restaurant_id = restaurant.id
+
+    -- 4.5 Get the average stars by restaurant. The result should have the restaurant name and its average star rating.
+    -- SELECT AVG(stars), name
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- ON restaurant_id = restaurant.id
+    -- GROUP BY name
+
+    -- 4.6 Get the number of reviews written for each restaurant. The result should have the restaurant name and its review count.
+    -- SELECT name, COUNT(review) AS review count
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- ON restaurant_id = restaurant.id
+    -- GROUP BY name
+
+    -- 4.7 List all the reviews along with the restaurant, and the reviewer's name. The result should have the restaurant name, the review text, and the reviewer name. Hint: you will need to do a three-way join - i.e. joining all three tables together.
+    -- SELECT restaurant.name, review.review, reviewer.name
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- ON restaurant_id = restaurant.id
+    -- INNER JOIN reviewer
+    -- ON reviewer_id = reviewer.id
+
+    -- 4.8 Get the average stars given by each reviewer. (reviewer name, average star rating)
+    -- SELECT name, AVG(stars)
+    -- FROM review
+    -- INNER JOIN reviewer
+    -- ON reviewer_id = reviewer.id
+    -- GROUP BY name
+
+    -- 4.9 Get the lowest star rating given by each reviewer. (reviewer name, lowest star rating)
+    -- SELECT name, MIN(stars)
+    -- FROM review
+    -- INNER JOIN reviewer
+    -- ON reviewer_id = reviewer.id
+    -- GROUP BY name
+
+    -- 4.10 Get the number of restaurants in each category. (category name, restaurant count)
+    -- SELECT category, COUNT(*)
+    -- FROM restaurant
+    -- GROUP BY category
+
+    -- 4.11 Get number of 5 star reviews given by restaurant. (restaurant name, 5-star count)
+    -- SELECT name, COUNT(*)
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- ON restaurant_id = restaurant.id
+    -- WHERE stars = 5
+    -- GROUP BY name
+    
+    -- 4.12 Get the average star rating for a food category. (category name, average star rating)
+    -- SELECT category, AVG(stars)
+    -- FROM review
+    -- INNER JOIN restaurant
+    -- On restaurant_id = restaurant.id
+    -- GROUP BY category
+
+-- 5. Projects DB Exercise
+
+
+
+
